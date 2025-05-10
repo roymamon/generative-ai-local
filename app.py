a8fd4ed1304dbe104cddbe7a40b151031a732a85
Roy Mamon <mamon8521@gmail.com>
2025-05-11 00:09:47 +0300

    comment

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 client = AzureOpenAI(
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_version="2024-12-01-preview"  # Make sure this matches your deployment version
+    api_version="2024-12-01-preview" 
 )
 
 deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
